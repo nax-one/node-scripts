dc348481807305fe0d5a798fbbbe6f4e057bf097
includeleec <includeleec@gmail.com>
2020-05-28 11:23:53 +0800

    refractor mint

--- mint.js
+++ mint.js
@@ -1,33 +1,36 @@
 const Nebulas = require("nebulas");
-const fs = require("fs");
-var _ = require("lodash");
-const moment = require("moment");
 
-const {
-  initNeb,
-  convert2nas,
-  convert2nax,
-  getAccState,
-  getNebState,
-  callNode,
-} = require("./lib/nebulas");
+// const {
+//   initNeb,
+//   convert2nas,
+//   convert2nax,
+//   getAccState,
+//   getNebState,
+//   callNode,
+// } = require("./lib/nebulas");
 
-const { clear_log, log, period2Time } = require("./lib/utils");
+// const { clear_log, log, period2Time } = require("./lib/utils");
 
-const Neb = Nebulas.Neb;
-const neb = new Neb();
+//
 
-initNeb(neb, "mainnet");
+import { call } from "./lib/nebCall";
+import _ from "lodash";
 
-const log_file = `data.md`;
+const { Log } = require("./lib/log");
+const { convert2nax, period2Time } = require("./lib/nebUtil");
+// import { contract } from "./config/contract";
+
+// const Neb = Nebulas.Neb;
+// const neb = new Neb();
+
+// initNeb(neb, "mainnet");
 
 const dayPeriodDuration = 27;
 
 run();
 
 async function run() {
-  const sysInfo = await callNode("getSystemInfo");
-  console.log("currentPeriod", sysInfo.currentPeriod);
+  const sysInfo = await call("getSystemInfo");
 
   let latestPeriod = sysInfo.currentPeriod;
   let startPeriod = latestPeriod - dayPeriodDuration;
@@ -37,42 +40,38 @@ async function run() {
 
   // create day period list
   const periodList = getPeriodList(startPeriod, latestPeriod);
+  let remainList = Array.from(periodList);
 
-  console.log(periodList);
+  // log.log(periodList);
 
   let nodeList = [];
 
+  // init log
+  const log = new Log(`./logs/mint/${startPeriod}_${latestPeriod - 1}.csv`);
+  log.clear();
+
   // clear log
-  clear_log(log_file);
-  log(`\nNebulas Node Mint Statics`, log_file);
-  log(
-    `\n星云节点平台出块统计\n如需获取每日信息，请加小助手微信 naxone01`,
-    log_file
-  );
-  log(
-    `\nStatistic period: ${startPeriod}(${startTime}) ~ ${
+  log.write(`\nNebulas Node Mint Statics`);
+  log.write(`星云节点平台出块统计\n如需获取每日信息，请加小助手微信 naxone01`);
+  log.write(
+    `Statistic period: ${startPeriod}(${startTime}) ~ ${
       latestPeriod - 1
-    }(${latestTime})`,
-    log_file
+    }(${latestTime})`
   );
-  log(`\nPowered by Nax.One`, log_file);
-  log(`\n\n---------------------------------`, log_file);
-  log(
-    `\n\n| node name | node id | rank | vote nax | mint blocks | mint nas |`,
-    log_file
-  );
-  log(
-    `\n| ----------- | ----------- | ----------- | ----------- | ----------- | ----------- |`,
-    log_file
-  );
+  log.write(`\nPowered by Nax.One`);
+  log.line("=");
+  log.write(`rank,node name,node id,vote nax,mint blocks,mint nas`);
 
   await Promise.all(
     periodList.map(async (p) => {
-      const blockData = await callNode("getBlockData", [p]);
-      console.log(`\nperiod: ${p}`);
-      console.log(`\n\n------------------------------`);
+      const blockData = await call("getBlockData", [p]);
 
-      // console.log(blockData);
+      log.log(`period: ${p}`);
+
+      _.remove(remainList, (prd) => p === prd);
+      let fetch_record_progress =
+        ((periodList.length - remainList.length) / periodList.length) * 100;
+      log.log(`fetch progress: ${fetch_record_progress.toFixed(2)}%`);
 
       blockData.forEach((bd) => {
         const findInNodes = nodeList.find((n) => n.id === bd["node"]["id"]);
@@ -112,11 +111,9 @@ async function run() {
   nodeList
     .sort((a, b) => a.rank - b.rank)
     .map((n) => {
-      log(
-        `\n| \`${n.name}\` | ${n.id} | ${n.rank} | ${n.vote} NAX | ${
-          n.totalMint
-        } | ${(n.totalMint * 1.189).toFixed(2)} NAS |`,
-        log_file
+      const totalMintNas = (n.totalMint * 1.189).toFixed(2);
+      log.write(
+        `${n.rank},""${n.name}",${n.id},"${n.vote} NAX",${n.totalMint},${totalMintNas} NAS`
       );
     });
 }
