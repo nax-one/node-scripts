ae9765d66bfcefd2191df044893dc191c040d4db
includeleec <includeleec@gmail.com>
2020-05-31 21:49:04 +0800

    add govern js

--- src/govern.js
+++ src/govern.js
@@ -13,7 +13,30 @@ const {
 const { datetime } = require("../lib/utils");
 import { contract } from "../config/contract";
 
-async function getUserNAXWithdrawList(addr) {
-  const res = await call("getUserNAXWithdrawList", [addr]);
-  return res;
+run();
+
+async function run() {
+  getGovAllNodesData(0);
+  getGovAllNodesData(1);
+  getGovAllNodesData(2);
+}
+
+async function getGovAllNodesData(govPeriod) {
+  const file = `./logs/govern/${govPeriod}_gov_nodes.md`;
+  const log = new Log(file);
+
+  log.clear();
+  const govNodes = await call(
+    "getGovAllNodesData",
+    [govPeriod],
+    contract["mainnet"]["govern"]
+  );
+
+  const totalNodes = govNodes.length;
+
+  log.write(`govern period: ${govPeriod}, total nodes: ${totalNodes}`);
+
+  for (let node of govNodes) {
+    log.write(`${node.node} ${node.count}`);
+  }
 }
